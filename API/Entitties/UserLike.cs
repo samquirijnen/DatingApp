d5d5cec1418eae3c3409fbbd732506e274290951
Sam Quirijnen <R0781463@student.thomasmore.be>
2022-01-23 12:34:25 +0100

    AddLikesFunction

---
+++ API/Entitties/UserLike.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace API.Entitties
+{
+    public class UserLike
+    {
+        public AppUser SourceUser { get; set; }
+        public int SourceUserId { get; set; }
+        public AppUser LikedUser { get; set; }
+        public int LikedUserId { get; set; }
+    }
+}
---
+++ API/Interfaces/ILikesRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using API.DTO;
+using API.Entitties;
+using API.Helpers;
+
+namespace API.Interfaces
+{
+    public interface ILikesRepository
+    {
+        Task<UserLike> GetUserLike(int sourceUserId, int likedUserId);
+        Task<AppUser> GetUserWithLikes(int userId);
+        Task<PagedList<LikeDto>> GetUsersLikes(LikesParams likesParams);
+    }
+}
