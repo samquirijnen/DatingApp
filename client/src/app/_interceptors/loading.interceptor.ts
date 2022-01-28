4dfb9b2ad67dbe0ee7f957e13536101db5608bac
Sam Quirijnen <R0781463@student.thomasmore.be>
2022-01-28 19:07:18 +0100

    change for deployment

--- client/src/app/_interceptors/loading.interceptor.ts
+++ client/src/app/_interceptors/loading.interceptor.ts
@@ -5,7 +5,7 @@ import {
   HttpEvent,
   HttpInterceptor
 } from '@angular/common/http';
-import { delay, finalize, Observable } from 'rxjs';
+import {finalize, Observable } from 'rxjs';
 import { BusyService } from '../_services/busy.service';
 
 @Injectable()
@@ -16,11 +16,9 @@ export class LoadingInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     this.busyService.busy()
     return next.handle(request).pipe(
-      delay(1000),
       finalize(()=>{
         this.busyService.idle()
       })
-
     );
   }
 }
